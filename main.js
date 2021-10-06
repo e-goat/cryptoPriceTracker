377c66f98781f1b124eb6cb83fe9da9b95108a95
Sam Smith <56628021+e-goat@users.noreply.github.com>
2021-10-06 22:42:02 +0300

    CoinTracker v0.2

--- main.js
+++ main.js
@@ -1,67 +1,72 @@
-(function($){
-    $(document).ready(()=>{
-        getDailyPrices();
-    });
+$(document).ready(function () {
+    getDailyPrices(); 
+    createTable(); 
+});
+var output;
+var dateOfCreation = '';
+let coinMetas = [];
+let allCoinsArray = [];
+var ajaxCalls = {};
+let newCount = 0;
 
-    
-    function getDailyPrices() {
-        var coinID = ['bitcoin', 'ethereum', 'cardano', 'polkadot', 'dogecoin', 'shiba-inu', 'ripple','neo', 'stellar', 'solana'];
-        $($(coinID).get().reverse()).each((index,id)=>{
-            $.ajax({   
-                url: `https://api.coingecko.com/api/v3/coins/${id}?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true&days=1`,   
-                dataType: "json",
-                method: "GET",
-                statusCode: {
-                    404: function() {
-                        alert('Page was not found!');
-                    }
-                },
-                success: function(data) {
-                    var dateOfCreation = '';
-                    if (data.genesis_date !== null) {
-                        dateOfCreation = data.genesis_date;
-                    } else {
-                        dateOfCreation = "Not available";
-                    }
-                    var metas = {
+const coinID = ['bitcoin', 'ethereum', 'cardano', 'polkadot', 'dogecoin', 'shiba-inu', 'ripple','neo', 'stellar', 'solana'];
+
+function getDailyPrices() {
+    let request;
+    $($(coinID).get().reverse()).each((index,id)=>{
+        newCount++
+        ajaxCalls[newCount] = $.ajax({   
+            url: `https://api.coingecko.com/api/v3/coins/${id}?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true&days=1`,   
+            dataType: "json",
+            method: "GET",
+            statusCode: {
+                404: function() {
+                    alert('Page was not found!');
+                }
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        }).done(function(data){
+            if (data.genesis_date !== null) {
+                dateOfCreation = data.genesis_date;
+            } else {
+                dateOfCreation = "Not available";
+            }
+            
+            for (var i=0; i< data.tickers.length; i++){
+                if ( data.market_cap_rank !== null && data.tickers[i].target == 'USD') {
+                    coinMetas.push({
                         rank: data.market_cap_rank,
                         name: data.name,
                         logo: data.image.thumb,
                         vs_currency: data.tickers[i].target,
                         price: data.tickers[i].last,
-                        creationDate: dateOfCreation
-                   }
-                   console.log(metas.rank)
-                },
-                error: function(err) {
-                    console.log(err);
-                }
-            });
+                        creationDate: data.genesis_date
+                    });
+                    break;
+                }   
+            }
         });
-    } 
-      
-    //   var output = "";
-    //   var dateOfCreation = '';
-    //   if (data.genesis_date !== null) {
-    //       dateOfCreation = data.genesis_date;
-    //   } else {
-    //       dateOfCreation = "Not available";
-    //   }
-    //   for (var i=0; i< data.tickers.length; i++){
-    //       // console.log(data.tickers[i].target);
-    //       if ( data.market_cap_rank !== null && data.tickers[i].target == 'USD') {
-    //           output +=`
-    //               <tr class="coin-row">
-    //                 <th scope="row" class="table-dark">${data.market_cap_rank}</th>
-    //                 <th scope="row" class="table-dark ${data.name}-row"><img src="${data.image.thumb}" class="coin-icon"/> ${data.name}</th>
-    //                 <th scope="row" class="table-dark">${data.tickers[i].target}</th>
-    //                 <th scope="row" class="table-dark">${data.tickers[i].last}</th>
-    //                 <th scope="row" class="table-dark">${dateOfCreation}</th>
-    //               </tr>`;
-    //           break;
-    //       }
-    //   }
-    //   $(".price-list").slideDown(300, function () {
-    //       $( this ).append( output );
-    //   });
-});
+    });
+} 
+
+function createTable() {
+    console.log(newCount)
+    $.when(ajaxCalls[10]).done(()=>{
+        allCoinsArray.push(coinMetas);
+        for (var eachCoin = 0; eachCoin < newCount; eachCoin++){
+            output +=`
+            <tr class="coin-row">
+              <th scope="row" class="table-dark">${allCoinsArray[0][eachCoin].rank}</th>
+              <th scope="row" class="table-dark ${allCoinsArray[0][eachCoin].name}-row"><img src="${allCoinsArray[0][eachCoin].logo}" class="coin-icon"/> ${allCoinsArray[0][eachCoin].name}</th>
+              <th scope="row" class="table-dark">${allCoinsArray[0][eachCoin].vs_currency}</th>
+              <th scope="row" class="table-dark">${allCoinsArray[0][eachCoin].price}</th>
+              <th scope="row" class="table-dark">${allCoinsArray[0][eachCoin].creationDate}</th>
+            </tr>`;  
+            $(".price-list").slideDown(300, function () {
+                $( this ).append( output );
+            });
+        }
+    });
+}
