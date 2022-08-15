51d53cb8e1a38ec3e751db342aada5906551d1b8
e-goat <duchevmartin@gmail.com>
2022-08-15 19:42:31 +0300

    Market cap added and big numbers are now handled

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 jQuery(function () {
     'use strict'
 
-    let output          = null,
-        newCount        = null,
-        coinID          = [],
-        ajaxCallObj     = {},
-        coinMeta        = [],
-        allCoinsArray   = [];
+    let 
+    output        = null,
+    newCount      = null,
+    coinID        = [],
+    ajaxCallObj   = {},
+    coinMeta      = [],
+    allCoinsArray = [];
 
     //Sort array by rank
     let sortByRank = (array, key) => {
@@ -31,8 +32,9 @@ jQuery(function () {
             'Access-Control-Allow-Origin':'cors',
         },
         success: response => {
-            let coinsLength = response.length,
-                data        = response;
+            let 
+            coinsLength = response.length,
+            data        = response;
 
             return [...Array(coinsLength)].forEach((element, index) => {
                 coinID.push( data[index].id );
@@ -71,25 +73,32 @@ jQuery(function () {
                 }
             }).done((data) => {
                 newCount++
-                for (var i=0; i< data.tickers.length; i++){
+                for (var i=0; i < data.tickers.length; i++){
                     if ( data.market_cap_rank !== null && data.tickers[i].target == 'USD') {
                         coinMeta.push({
-                            rank:           data.market_cap_rank,
-                            name:           data.name,
-                            logo:           data.image.thumb,
-                            trade_on:       data.tickers[i].trade_url,
-                            vs_currency:    data.tickers[i].target,
-                            price:          data.tickers[i].last,
-                            creationDate:   data.genesis_date,
-                            ex_name:        data.tickers[i].market.name,
-                        });                    
+                            rank:         data.market_cap_rank,
+                            name:         data.name,
+                            logo:         data.image.thumb,
+                            trade_on:     data.tickers[i].trade_url,
+                            vs_currency:  data.tickers[i].target,
+                            price:        data.tickers[i].last,
+                            creationDate: data.genesis_date,
+                            ex_name:      data.tickers[i].market.name,
+                            market_cap:   data.market_data.market_cap['usd'],
+                            total_supply: data.circulating_supply
+                        });
                         break;
                     }   
                 }
             });
         });
         return createTable();
-    } 
+    }
+
+    // HANDLE BIG NUMBERS WITH COMMAS
+    let bigNumHelper = (num = null ) => {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
 
     //Dynamically create table rows
     let createTable = () => {
@@ -99,21 +108,28 @@ jQuery(function () {
                 sortByRank(allCoinsArray[0],"rank");
                 for (var eachCoin = 0; eachCoin < newCount; eachCoin++){
                     if (allCoinsArray[0][eachCoin] != undefined) {
-                        let rank        = allCoinsArray[0][eachCoin].rank,
-                            name        = allCoinsArray[0][eachCoin].name,
-                            price       = allCoinsArray[0][eachCoin].price,
-                            logo        = allCoinsArray[0][eachCoin].logo,
-                            origin_date = allCoinsArray[0][eachCoin].creationDate,
-                            currency    = allCoinsArray[0][eachCoin].vs_currency,
-                            trade_on    = allCoinsArray[0][eachCoin].trade_on,
-                            exchange    = allCoinsArray[0][eachCoin].ex_name;
-                        
+                        let 
+                        rank         = allCoinsArray[0][eachCoin].rank,
+                        name         = allCoinsArray[0][eachCoin].name,
+                        price        = allCoinsArray[0][eachCoin].price,
+                        logo         = allCoinsArray[0][eachCoin].logo,
+                        origin_date  = allCoinsArray[0][eachCoin].creationDate,
+                        currency     = allCoinsArray[0][eachCoin].vs_currency,
+                        trade_on     = allCoinsArray[0][eachCoin].trade_on,
+                        exchange     = allCoinsArray[0][eachCoin].ex_name,
+                        market_cap   = allCoinsArray[0][eachCoin].market_cap,
+                        total_supply = allCoinsArray[0][eachCoin].total_supply;
+
+                        // Transform ternary operator, not convenient enough but it works for US only.
+                        currency == 'USD' ? currency = '$' : '';
+
                         output += 
                         `<tr class="coin-row">
                             <th scope="row" class="table-dark rank">${rank}</th>
                             <th scope="row" class="table-dark name-${name}" value="name"><img src="${logo}" class="coin-icon"/> ${name}</th>
                             <th scope="row" class="table-dark price">${currency} ${price}</th>
                             <th scope="row" class="table-dark creation-date">${origin_date != null ? origin_date : "N/A" }</th>
+                            <th scope="row" class="table-dark market-cap">${currency} ${bigNumHelper(market_cap)}</th>
                             <th scope="row" class="table-dark trade-on">
                                 <a href="${trade_on}" class="button" target="_blank">Trade on ${exchange}</a>
                             </th>
